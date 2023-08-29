47c75d3253b5ec1b15c6bec0e42d94dcf58d982a
foru17 <i@luolei.org>
2023-08-29 22:17:16 +0800

    feature:新增谷歌统计阅读量展示

--- docs/.vitepress/theme/components/ArticleMeta.vue
+++ docs/.vitepress/theme/components/ArticleMeta.vue
@@ -17,31 +17,35 @@
     router.go(`/?category=${category}`);
   };
   const pageHits = ref<number>(0);
-
+  const isPageHitsFetched = ref<boolean>(false);
   const fetchPageHits = async () => {
     try {
-      const response = await fetch("https://stat.luolei.org/api/ga");
-      const data = await response.json();
+      const response = await fetch(
+        `https://st.luolei.org/ga?page=${route.path}`
+      );
+      const { data } = await response.json();
       const currentPageHit = data.find(
-        (item: any) => item.page === `${route.path}/`
+        (item: any) => item.page === `${route.path}`
       );
 
       if (currentPageHit) {
         pageHits.value = currentPageHit.hit;
       }
+      isPageHitsFetched.value = true;
     } catch (error) {
       console.error("Error fetching page hits:", error);
     }
   };
 
   onMounted(() => {
-    // fetchPageHits();
+    fetchPageHits();
   });
 
   watch(
     () => router.route.data.relativePath,
     () => {
-      // fetchPageHits();
+      isPageHitsFetched.value = false;
+      fetchPageHits();
     }
   );
 </script>
@@ -60,7 +64,7 @@
           </h2>
 
           <!-- 如果categories 有值,则遍历渲染 -->
-          <div v-if="categories" class="mt-2">
+          <!-- <div v-if="categories" class="mt-2">
             <span
               v-for="(category, index) of categories"
               :key="index"
@@ -68,13 +72,32 @@
               class="hidden inline-block border bg-transparent rounded-xl px-3 py-1 text-sm font-semibold text-white mr-2">
               {{ category }}
             </span>
-          </div>
+          </div> -->
           <p class="inline-block mt-2 text-base text-slate-200 mr-5">
             发布时间: {{ date }}
           </p>
-          <!-- <p class="block md:inline-block mt-2 text-base text-slate-200">
-            阅读量: {{ pageHits }}
-          </p> -->
+
+          <p class="block md:inline-block mt-2 text-base text-slate-200">
+            阅读量:
+            <i v-if="isPageHitsFetched" class="not-italic">{{ pageHits }}</i
+            ><span v-if="!isPageHitsFetched" role="status" class="inline-block">
+              <svg
+                aria-hidden="true"
+                class="w-3 h-3 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor" />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill" />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </span>
+          </p>
+
           <p class="mt-2 text-white"></p>
         </div>
       </div>
