9a7633b7b23814f7a15995ed18c0eaa251bbb365
foru17 <i@luolei.org>
2024-02-07 10:07:22 +0800

    fix:修正clientonly

--- docs/.vitepress/theme/components/ArticleCard.vue
+++ docs/.vitepress/theme/components/ArticleCard.vue
@@ -1,120 +1,143 @@
 <script setup lang="ts">
-  import { data as posts } from "../posts.data.js";
-  import { useData, withBase, useRouter } from "vitepress";
-  const { frontmatter } = useData();
-  import { watch, nextTick, ref, onMounted, computed } from "vue";
-  import { getPreviewImage, getFormatNumber } from "../utils";
-  const props = defineProps<{
-    url: string;
-    title: string;
-    cover: string;
-    date: Object;
-    categories: string[];
-    hit: number;
-    isArticleListHitsFetched: boolean;
-  }>();
+import { data as posts } from "../posts.data.js";
+import { useData, withBase, useRoute, useRouter } from "vitepress";
+const { frontmatter } = useData();
+import { watch, nextTick, ref, onMounted, computed } from "vue";
+import { getPreviewImage, getFormatNumber } from "../utils";
+const props = defineProps<{
+  url: string;
+  title: string;
+  cover: string;
+  date: Object;
+  categories: string[];
+  hit: number;
+  isArticleListHitsFetched: boolean;
+}>();
 
-  const hotArticleViews = 5000;
-  const router = useRouter();
-  let timeoutHandle = null;
-  const imgRef = ref<HTMLImageElement | null>(null);
+const hotArticleViews = 5000;
+const route = useRoute();
+const router = useRouter();
+let timeoutHandle = null;
+const imgRef = ref<HTMLImageElement | null>(null);
 
-  const imageLoaded = ref(false);
-  const imageError = ref(false);
+const imageLoaded = ref(false);
+const imageError = ref(false);
 
-  const onImageLoad = () => {
-    imageError.value = false;
-    imageLoaded.value = true;
-  };
+const onImageLoad = () => {
+  imageError.value = false;
+  imageLoaded.value = true;
+};
 
-  const onImageError = () => {
-    imageError.value = true;
-    imageLoaded.value = true; // 也设置图片为已加载，隐藏加载动画
-  };
+const onImageError = () => {
+  imageError.value = true;
+  imageLoaded.value = true; // 也设置图片为已加载，隐藏加载动画
+};
 
-  const articleUrl = computed(() => {
-    return withBase(props.url);
-  });
-  const previewImageUrl = computed(() => {
-    if (!props.cover) {
-      console.error("Cover image URL is not provided!");
-      return "";
+const articleUrl = computed(() => {
+  return withBase(props.url);
+});
+const previewImageUrl = computed(() => {
+  if (!props.cover) {
+    console.error("Cover image URL is not provided!");
+    return "";
+  }
+
+  return getPreviewImage(props.cover);
+});
+const retryLoadImage = () => {
+  // 清除旧的超时句柄
+  clearTimeout(timeoutHandle);
+  // 设置超时逻辑
+  timeoutHandle = setTimeout(() => {
+    if (!imageLoaded.value) {
+      onImageError();
     }
-   
-    return getPreviewImage(props.cover);
-  });
-  const retryLoadImage = () => {
-    // 清除旧的超时句柄
-    clearTimeout(timeoutHandle);
-    // 设置超时逻辑
-    timeoutHandle = setTimeout(() => {
-      if (!imageLoaded.value) {
-        onImageError();
-      }
-    }, 15000);
-  };
+  }, 15000);
+};
 
-  const goCategory = (category: string) => {
-    router.go(`?category=${category}`);
-  };
+const goCategory = (category: string) => {
+  router.go(`?category=${category}`);
+};
 
-  onMounted(() => {
-    // 当组件被挂载后
-    nextTick(() => {
-      if (imgRef.value?.complete) {
-        imageLoaded.value = true;
-      }
-      retryLoadImage();
-    });
+const hasNavQuery = computed(() => {
+  // 如果url中有category或者 page 参数,则返回true
+  const { searchParams } = new URL(window.location.href!);
+  return searchParams.has("category") || searchParams.has("page");
+});
+
+onMounted(() => {
+  // 当组件被挂载后
+  nextTick(() => {
+    if (imgRef.value?.complete) {
+      imageLoaded.value = true;
+    }
+    retryLoadImage();
   });
+});
 </script>
 
 <template>
   <div
-    class="flex-1 h-64 overflow-hidden duration-300 ease-in-out bg-white rounded-t shadow-lg dark:bg-zinc-800 hover:shadow-2xl">
-    <ClientOnly>
-    <a
-      :href="articleUrl"
-      class="flex flex-wrap no-underline hover:no-underline">
-      <div
-        class="relative w-full overflow-hidden h-60 md:h-40 ld:h-40 bg-zinc-100 dark:bg-neutral-900">
-        <img
-          ref="imgRef"
-          :src="previewImageUrl"
-          @load="onImageLoad"
-          @error="onImageError"
-          :class="{
-            'opacity-0': !imageLoaded,
-            'opacity-100': imageLoaded && !imageError,
-            'opacity-0 delay-0': imageLoaded && imageError,
-          }"
-          class="absolute top-0 left-0 object-cover w-full h-full duration-300 ease-in rounded-t hover:scale-105" />
-      
-        <!-- 如果分类中有zuoluotv或者视频,则展示youtube图标 -->
-        <div>
-          <svg
-            v-if="categories.includes('zuoluotv')"
-            class="absolute left-6 bottom-2 h-7 w-7 md:h-5 md:w-5"
-            viewBox="0 0 473.931 473.931"
-            xml:space="preserve">
-            <circle
-              style="fill: #d42428"
-              cx="236.966"
-              cy="236.966"
-              r="236.966" />
-            <path
-              style="fill: #cc202d"
-              d="M404.518,69.38c92.541,92.549,92.549,242.593,0,335.142c-92.541,92.541-242.593,92.545-335.142,0
-	L404.518,69.38z" />
-            <path
-              style="fill: #ba202e"
-              d="M470.321,277.964L310.843,118.487l-12.864,12.864l-12.864-12.864l-14.099,14.099l9.47,9.47
+    class="flex-1 h-64 overflow-hidden duration-300 ease-in-out bg-white rounded-t shadow-lg dark:bg-zinc-800 hover:shadow-2xl"
+  >
+    <div class="flex flex-wrap no-underline hover:no-underline">
+      <ClientOnly>
+        <a
+          :href="articleUrl"
+          class="relative w-full overflow-hidden h-60 md:h-40 ld:h-40 bg-zinc-100 dark:bg-neutral-900"
+        >
+          <img
+            ref="imgRef"
+            :src="previewImageUrl"
+            @load="onImageLoad"
+            @error="onImageError"
+            :class="[
+              'absolute',
+              'top-0',
+              'left-0',
+              'object-cover',
+              'w-full',
+              'h-full',
+              'duration-300',
+              'ease-in',
+              'rounded-t',
+              'hover:scale-105',
+              {
+                'opacity-0': !imageLoaded || (imageLoaded && imageError),
+                'opacity-100': imageLoaded && !imageError,
+              },
+            ]"
+          />
+
+          <!-- 如果分类中有zuoluotv或者视频,则展示youtube图标 -->
+          <div>
+            <svg
+              v-if="categories.includes('zuoluotv')"
+              class="absolute left-6 bottom-2 h-7 w-7 md:h-5 md:w-5"
+              viewBox="0 0 473.931 473.931"
+              xml:space="preserve"
+            >
+              <circle
+                style="fill: #d42428"
+                cx="236.966"
+                cy="236.966"
+                r="236.966"
+              />
+              <path
+                style="fill: #cc202d"
+                d="M404.518,69.38c92.541,92.549,92.549,242.593,0,335.142c-92.541,92.541-242.593,92.545-335.142,0
+	L404.518,69.38z"
+              />
+              <path
+                style="fill: #ba202e"
+                d="M470.321,277.964L310.843,118.487l-12.864,12.864l-12.864-12.864l-14.099,14.099l9.47,9.47
 	l-3.091,3.091l-24.557-24.557l-1.048,1.055l-33.092-33.092l-14.099,14.099l3.858,3.858l-5.665,5.665l-23.854-23.854l-9.889,9.889
 	l23.255,56.003l-10.473,42.997l28.632,28.639l-72.418,5.066l-1.096,125.667l116.537,116.679
-	C362.678,465.505,451.836,383.833,470.321,277.964z" />
-            <path
-              style="fill: #ffffff"
-              d="M321.724,291.91h-15.289l0.075-8.875c0-3.948,3.237-7.169,7.199-7.169h0.98
+	C362.678,465.505,451.836,383.833,470.321,277.964z"
+              />
+              <path
+                style="fill: #ffffff"
+                d="M321.724,291.91h-15.289l0.075-8.875c0-3.948,3.237-7.169,7.199-7.169h0.98
 	c3.963,0,7.214,3.222,7.214,7.169L321.724,291.91z M264.404,272.89c-3.865,0-7.038,2.608-7.038,5.796v43.161
 	c0,3.188,3.173,5.781,7.038,5.781c3.895,0,7.068-2.593,7.068-5.781v-43.165C271.472,275.498,268.299,272.89,264.404,272.89z
 	 M357.473,248.752v82.102c0,19.697-17.077,35.809-37.96,35.809H163.444c-20.887,0-37.96-16.116-37.96-35.809v-82.102
@@ -139,53 +162,68 @@
 	c-1.594-2.357-2.705-5.227-3.375-8.572c-0.681-3.349-1.003-8.374-1.003-15.079v-20.247H217.408z M231.484,176.716
 	c0,4.325,3.229,7.861,7.154,7.861s7.132-3.532,7.132-7.861v-40.71c0-4.325-3.207-7.861-7.132-7.861s-7.154,3.532-7.154,7.861
 	V176.716z M181.722,197.269h16.969l0.015-58.648l20.045-50.241h-18.555l-10.657,37.324l-10.81-37.421h-18.357l21.324,50.372
-	L181.722,197.269z" />
-          </svg>
-        </div>
-        <div
-          v-if="!imageLoaded || imageError"
-          :class="{ 'animate-pulse': !imageLoaded }"
-          class="flex p-2 mt-6 space-x-4">
-          <span v-if="!imageError" class="relative flex w-10 h-10">
-            <span
-              class="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-sky-100"></span>
-            <span
-              class="relative inline-flex w-10 h-10 rounded-full bg-slate-200 dark:bg-slate-600"></span>
-          </span>
-
+	L181.722,197.269z"
+              />
+            </svg>
+          </div>
           <div
-            v-if="imageError"
-            class="md:block w-0 h-0 mt-1 border-l-[20px] border-l-transparent border-t-[30px] border-t-slate-200 dark:border-t-slate-600 border-r-[20px] border-r-transparent"></div>
-          <div class="flex-1 py-1 space-y-6">
+            v-if="!imageLoaded || imageError"
+            :class="{ 'animate-pulse': !imageLoaded }"
+            class="flex p-2 mt-6 space-x-4"
+          >
+            <span v-if="!imageError" class="relative flex w-10 h-10">
+              <span
+                class="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-sky-100"
+              ></span>
+              <span
+                class="relative inline-flex w-10 h-10 rounded-full bg-slate-200 dark:bg-slate-600"
+              ></span>
+            </span>
+
             <div
-              class="h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"></div>
-            <div class="space-y-3">
-              <div class="grid grid-cols-3 gap-4">
-                <div
-                  class="h-8 col-span-2 rounded md:h-4 bg-slate-200 dark:bg-slate-600"></div>
-                <div
-                  class="h-8 col-span-1 rounded md:h-4 bg-slate-200 dark:bg-slate-600"></div>
-              </div>
+              v-if="imageError"
+              class="md:block w-0 h-0 mt-1 border-l-[20px] border-l-transparent border-t-[30px] border-t-slate-200 dark:border-t-slate-600 border-r-[20px] border-r-transparent"
+            ></div>
+            <div class="flex-1 py-1 space-y-6">
               <div
-                class="h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"></div>
+                class="h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+              ></div>
+              <div class="space-y-3">
+                <div class="grid grid-cols-3 gap-4">
+                  <div
+                    class="h-8 col-span-2 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+                  ></div>
+                  <div
+                    class="h-8 col-span-1 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+                  ></div>
+                </div>
+                <div
+                  class="h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+                ></div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </a>
+      </ClientOnly>
       <div class="w-full px-6 mt-5">
-        <p
-          class="h-auto text-base antialiased font-medium text-gray-800 break-normal md:h-12 sd:text-lg md:text-base dark:text-slate-300 line-clamp-2 font-fira">
-          {{ title }}
-        </p>
+        <ClientOnly>
+          <a
+            :href="articleUrl"
+            class="h-auto text-base antialiased font-medium text-gray-800 break-normal md:h-12 sd:text-lg md:text-base dark:text-slate-300 line-clamp-2 font-fira"
+          >
+            {{ title }}
+          </a>
+        </ClientOnly>
       </div>
-    </a>
-    </ClientOnly>
+    </div>
   </div>
   <div
-    class="flex-none h-12 px-6 py-3 mt-auto overflow-hidden bg-white rounded-t-none rounded-b shadow-lg dark:bg-zinc-800">
+    class="flex-none h-12 px-6 py-3 mt-auto overflow-hidden bg-white rounded-t-none rounded-b shadow-lg dark:bg-zinc-800"
+  >
     <div class="flex items-center justify-between">
       <p
-        class="text-sm text-gray-400 dark:text-slate-400 sd:text-sm md:text-sm">
+        class="text-sm text-gray-400 dark:text-slate-400 sd:text-sm md:text-sm"
+      >
         <svg
           class="h-3 w-3 inline-block -mt-0.5"
           width="24"
@@ -195,7 +233,8 @@
           stroke="currentColor"
           fill="none"
           stroke-linecap="round"
-          stroke-linejoin="round">
+          stroke-linejoin="round"
+        >
           <path stroke="none" d="M0 0h24v24H0z" />
           <rect x="4" y="5" width="16" height="16" rx="2" />
           <line x1="16" y1="3" x2="16" y2="7" />
@@ -203,7 +242,7 @@
           <line x1="4" y1="11" x2="20" y2="11" />
           <rect x="8" y="15" width="2" height="2" />
         </svg>
-        {{ date.formatShowDate }} 
+        {{ date.formatShowDate }}
       </p>
 
       <div class="flex items-center justify-items-end">
@@ -213,13 +252,16 @@
           class="w-3 h-3 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
           viewBox="0 0 100 101"
           fill="none"
-          xmlns="http://www.w3.org/2000/svg">
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor" />
+            fill="currentColor"
+          />
           <path
             d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill" />
+            fill="currentFill"
+          />
         </svg>
 
         <svg
@@ -227,28 +269,33 @@
           class="inline-block w-3 h-3 mr-1 text-gray-400 dark:text-slate-400"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor">
+          stroke="currentColor"
+        >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+          />
         </svg>
         <svg
           v-if="isArticleListHitsFetched && hit > hotArticleViews"
           class="-mt-0 text-red-400 icon dark:text-red-500 fill-blue-600"
           viewBox="0 0 1024 1024"
           width="14"
-          height="14">
+          height="14"
+        >
           <path
             d="M750.4 308.8c-4.8-4.8-12.8-1.6-12.8 4.8 0 40 16 97.6-49.6 123.2 0 0 9.6-268.8-281.6-310.4-6.4-1.6-11.2 4.8-9.6 11.2 11.2 38.4 32 156.8-72 230.4-6.4 4.8-11.2 8-17.6 12.8-30.4-38.4-75.2-52.8-94.4-56-3.2-1.6-6.4 3.2-4.8 6.4 28.8 80-27.2 168-27.2 168h1.6c-36.8 54.4-51.2 118.4-25.6 209.6 0 0 51.2 152 243.2 188.8l-6.4-6.4s-116.8-118.4-65.6-256c28.8-73.6 89.6-113.6 89.6-113.6s-28.8 96 38.4 124.8c0 0-11.2-144 112-195.2 0 0-14.4 75.2 48 163.2 64 89.6 107.2 200 22.4 272-3.2 1.6-4.8 4.8-8 6.4 94.4-24 153.6-78.4 193.6-132.8 60.8-84.8 88-275.2-73.6-451.2z"
             fill="currentColor"
-            p-id="10161"></path>
+            p-id="10161"
+          ></path>
         </svg>
 
         <p
@@ -257,7 +304,7 @@
           :class="{
             'text-red-400 dark:text-red-500': hit > hotArticleViews,
           }"
-          >
+        >
           {{ getFormatNumber(hit) }}
         </p>
       </div>
