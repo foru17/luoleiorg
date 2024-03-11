dbb37cdd9a8febf64647c84d38173b3908cf0440
foru17 <i@luolei.org>
2024-03-11 22:25:06 +0800

    fix:修正文章和推特卡片跳转

--- docs/.vitepress/theme/components/TweetCard.vue
+++ docs/.vitepress/theme/components/TweetCard.vue
@@ -37,12 +37,20 @@ const goToTwitterProfile = () => {
   window.open(`https://twitter.com/${props.twitterId}`, "_blank");
 };
 
+const goToTweet = () => {
+  window.open(
+    `https://twitter.com/${props.twitterId}/status/${props.tweetId}`,
+    "_blank"
+  );
+};
+
 </script>
 
 <template>
   <div class="px-2 mb-12 tweet-card rounded-xl">
     <div
       class="relative m-auto flex h-full w-full max-w-[32rem] flex-col gap-2 overflow-hidden rounded-xl border p-4 backdrop-blur-md shadow-xl hover:shadow-2xl transition ease-in-out"
+      @click="goToTweet"
     >
       <div class="flex flex-row justify-between tracking-tight">
         <div class="flex items-center space-x-2">
@@ -102,11 +110,11 @@ const goToTwitterProfile = () => {
         class="my-0 border border-b-0 border-gray-200 dark:border-gray-600"
       ></div>
       <div class="flex mt-0 text-gray-500 dark:text-gray-400">
-        <div class="flex items-center mr-6">
+        <div class="flex items-center mr-6 hover:text-gray-600" @click="goToTweet">
           <svg viewBox="0 0 24 24" aria-hidden="true" class="w-auto h-5 fill-current"><g><path d="M1.751 10c0-4.42 3.584-8 8.005-8h4.366c4.49 0 8.129 3.64 8.129 8.13 0 2.96-1.607 5.68-4.196 7.11l-8.054 4.46v-3.69h-.067c-4.49.1-8.183-3.51-8.183-8.01zm8.005-6c-3.317 0-6.005 2.69-6.005 6 0 3.37 2.77 6.08 6.138 6.01l.351-.01h1.761v2.3l5.087-2.81c1.951-1.08 3.163-3.13 3.163-5.36 0-3.39-2.744-6.13-6.129-6.13H9.756z"></path></g></svg>
           <span class="ml-3">{{ comment }} </span>
         </div>
-        <div class="flex items-center mr-6">
+        <div class="flex items-center mr-6 hover:text-gray-600"  @click="goToTweet">
           <svg
             viewBox="0 0 24 24"
             aria-hidden="true"
