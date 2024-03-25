05aac1ce2dff6c5fded130bb379c67ddc2da462b
foru17 <i@luolei.org>
2024-03-25 22:27:25 +0800

    post:抗癌笔记:评论信息汇总

--- docs/.vitepress/theme/components/TweetCard.vue
+++ docs/.vitepress/theme/components/TweetCard.vue
@@ -72,7 +72,7 @@ const goToTweet = () => {
               :href="`https://twitter.com/${twitterId}`"
               target="_blank"
               rel="noreferrer"
-              class="flex items-center !font-semibold whitespace-nowrap"
+              class="flex items-center !font-semibold whitespace-nowrap !text-black dark:!text-slate-50"
               >{{ author }}</a
             >
             <div class="flex items-center space-x-1">
@@ -90,12 +90,8 @@ const goToTweet = () => {
           :href="`https://twitter.com/${twitterId}/status/${tweetId}`"
           target="_blank"
           rel="noreferrer"
-          ><svg viewBox="0 0 24 24" class="inline w-6 h-6 ml-1">
-            <path
-              fill="rgb(29,161,242)"
-              d="M23.643 4.937c-.835.37-1.732.62-2.675.733a4.67 4.67 0 002.048-2.578 9.3 9.3 0 01-2.958 1.13 4.66 4.66 0 00-7.938 4.25 13.229 13.229 0 01-9.602-4.868c-.4.69-.63 1.49-.63 2.342A4.66 4.66 0 003.96 9.824a4.647 4.647 0 01-2.11-.583v.06a4.66 4.66 0 003.737 4.568 4.692 4.692 0 01-2.104.08 4.661 4.661 0 004.352 3.234 9.348 9.348 0 01-5.786 1.995 9.5 9.5 0 01-1.112-.065 13.175 13.175 0 007.14 2.093c8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602a9.47 9.47 0 002.323-2.41z"
-            ></path></svg
-        ></a>
+          class="!text-current dark:!text-white"
+          ><svg viewBox="0 0 24 24" aria-hidden="true" class="inline w-6 h-6 ml-1 fill-current"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg></a>
       </div>
       <div class="text-base leading-normal tracking-tighter break-words">
         {{ content }}
