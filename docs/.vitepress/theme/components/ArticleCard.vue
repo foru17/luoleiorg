37f9254642840671ecab1d80e4c03cb05ec764f6
foru17 <i@luolei.org>
2024-09-17 17:48:01 +0800

    fix:修正feed的模糊图片

--- docs/.vitepress/theme/components/ArticleCard.vue
+++ docs/.vitepress/theme/components/ArticleCard.vue
@@ -78,13 +78,13 @@ onMounted(() => {
 
 <template>
   <div
-    class="flex-1 h-64 overflow-hidden duration-300 ease-in-out bg-white rounded-t shadow-lg dark:bg-zinc-800 hover:shadow-2xl"
+    class="overflow-hidden flex-1 h-64 bg-white rounded-t shadow-lg duration-300 ease-in-out dark:bg-zinc-800 hover:shadow-2xl"
   >
     <div class="flex flex-wrap no-underline hover:no-underline">
       <ClientOnly>
         <a
           :href="articleUrl"
-          class="relative w-full overflow-hidden h-60 md:h-40 ld:h-40 bg-zinc-100 dark:bg-neutral-900"
+          class="overflow-hidden relative w-full h-60 md:h-40 ld:h-40 bg-zinc-100 dark:bg-neutral-900"
         >
           <img
             ref="imgRef"
@@ -113,7 +113,7 @@ onMounted(() => {
           <div>
             <svg
               v-if="categories.includes('zuoluotv')"
-              class="absolute left-6 bottom-2 h-7 w-7 md:h-5 md:w-5"
+              class="absolute bottom-2 left-6 w-7 h-7 md:h-5 md:w-5"
               viewBox="0 0 473.931 473.931"
               xml:space="preserve"
             >
@@ -171,12 +171,12 @@ onMounted(() => {
             :class="{ 'animate-pulse': !imageLoaded }"
             class="flex p-2 mt-6 space-x-4"
           >
-            <span v-if="!imageError" class="relative flex w-10 h-10">
+            <span v-if="!imageError" class="flex relative w-10 h-10">
               <span
-                class="absolute inline-flex w-full h-full rounded-full opacity-75 animate-ping bg-sky-100"
+                class="inline-flex absolute w-full h-full bg-sky-100 rounded-full opacity-75 animate-ping"
               ></span>
               <span
-                class="relative inline-flex w-10 h-10 rounded-full bg-slate-200 dark:bg-slate-600"
+                class="inline-flex relative w-10 h-10 rounded-full bg-slate-200 dark:bg-slate-600"
               ></span>
             </span>
 
@@ -191,10 +191,10 @@ onMounted(() => {
               <div class="space-y-3">
                 <div class="grid grid-cols-3 gap-4">
                   <div
-                    class="h-8 col-span-2 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+                    class="col-span-2 h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
                   ></div>
                   <div
-                    class="h-8 col-span-1 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
+                    class="col-span-1 h-8 rounded md:h-4 bg-slate-200 dark:bg-slate-600"
                   ></div>
                 </div>
                 <div
@@ -205,7 +205,7 @@ onMounted(() => {
           </div>
         </a>
       </ClientOnly>
-      <div class="w-full px-6 mt-5">
+      <div class="px-6 mt-5 w-full">
         <ClientOnly>
           <a
             :href="articleUrl"
@@ -218,14 +218,14 @@ onMounted(() => {
     </div>
   </div>
   <div
-    class="flex-none h-12 px-6 py-3 mt-auto overflow-hidden bg-white rounded-t-none rounded-b shadow-lg dark:bg-zinc-800"
+    class="overflow-hidden flex-none px-6 py-3 mt-auto h-12 bg-white rounded-t-none rounded-b shadow-lg dark:bg-zinc-800"
   >
-    <div class="flex items-center justify-between">
+    <div class="flex justify-between items-center">
       <p
         class="text-sm text-gray-400 dark:text-slate-400 sd:text-sm md:text-sm"
       >
         <svg
-          class="h-3 w-3 inline-block -mt-0.5"
+          class="inline-block -mt-0.5 w-3 h-3"
           width="24"
           height="24"
           viewBox="0 0 24 24"
@@ -245,7 +245,7 @@ onMounted(() => {
         {{ date.formatShowDate }}
       </p>
 
-      <div class="flex items-center justify-items-end">
+      <div class="flex justify-items-end items-center">
         <svg
           v-if="!isArticleListHitsFetched"
           aria-hidden="true"
@@ -266,7 +266,7 @@ onMounted(() => {
 
         <svg
           v-if="isArticleListHitsFetched && hit <= hotArticleViews"
-          class="inline-block w-3 h-3 mr-1 text-gray-400 dark:text-slate-400"
+          class="inline-block mr-1 w-3 h-3 text-gray-400 dark:text-slate-400"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -315,7 +315,7 @@ onMounted(() => {
           v-for="(category, index) of categories.slice(0, 1)"
           :key="index"
           @click="goCategory(category)"
-          class="hidden inline-block px-2 py-0 mr-2 text-sm font-semibold text-gray-300 bg-transparent border rounded dark:text-slate-300">
+          class="inline-block hidden px-2 py-0 mr-2 text-sm font-semibold text-gray-300 bg-transparent rounded border dark:text-slate-300">
           {{ category }}
         </span>
       </div> -->
